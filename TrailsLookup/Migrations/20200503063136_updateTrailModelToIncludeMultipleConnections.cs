3d349c6554825e46b61adf22d4acb927e17e5d2f
Michelle Morin <morin2m@gmail.com>
2020-05-04 22:01:38 -0700

    add initial migration for trail model

---
+++ TrailsLookup/Migrations/20200503063136_updateTrailModelToIncludeMultipleConnections.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrailsLookup.Migrations
+{
+    public partial class updateTrailModelToIncludeMultipleConnections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trails",
+                columns: table => new
+                {
+                    TrailId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    GeoObjNumber = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Miles = table.Column<double>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Surface = table.Column<string>(nullable: true),
+                    FirstAdjoiningSegment1 = table.Column<int>(nullable: false),
+                    FirstAdjoiningSegment2 = table.Column<int>(nullable: true),
+                    FirstAdjoiningSegment3 = table.Column<int>(nullable: true),
+                    SecondAdjoiningSegment1 = table.Column<int>(nullable: false),
+                    SecondAdjoiningSegment2 = table.Column<int>(nullable: true),
+                    SecondAdjoiningSegment3 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trails", x => x.TrailId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trails");
+        }
+    }
+}
---
+++ TrailsLookup/Migrations/20200503221756_addFourthPossibleConnectingSegmentToModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrailsLookup.Migrations
+{
+    public partial class addFourthPossibleConnectingSegmentToModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FirstAdjoiningSegment4",
+                table: "Trails",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SecondAdjoiningSegment4",
+                table: "Trails",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstAdjoiningSegment4",
+                table: "Trails");
+
+            migrationBuilder.DropColumn(
+                name: "SecondAdjoiningSegment4",
+                table: "Trails");
+        }
+    }
+}
