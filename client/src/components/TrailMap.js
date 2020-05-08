a763d7238bf8a73efd0f75471c780d823eea959c
Michelle Morin <morin2m@gmail.com>
2020-05-08 11:19:05 -0700

    change color of trail segments and bind popups to each feature of geojson data

--- client/src/components/TrailMap.js
+++ client/src/components/TrailMap.js
@@ -10,13 +10,30 @@ import '../index.css';
 // `;
 
 export default function TrailMap() {
+
+  // const [activeTrail, setActiveTrail] = useState(null);
+
+  const featureStyle = {
+    color: '#749774'
+  }
+
+  const onEachFeature = (feature, layer) => {
+    const popupContent = `
+      <h4>${feature.properties.Name}</h4>
+      <p>${feature.properties.Miles} miles<p>
+      <p>${feature.properties.Type}<p>
+      <p>surface: ${feature.properties.Surface}<p>
+    `;
+    layer.bindPopup(popupContent);
+};
+
   return (
     <Map center={[45.5252, -122.7163]} zoom={13}>
       <TileLayer 
         url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
         maxZoom={20}
       />
-      <GeoJSON data={trailData} />
+      <GeoJSON data={trailData} style={featureStyle} onEachFeature={onEachFeature} />
     </Map>
   );
 }
