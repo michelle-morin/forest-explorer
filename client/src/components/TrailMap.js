3b2d0a91030d89044659cffa86c6f02566ec1da8
Michelle Morin <morin2m@gmail.com>
2020-05-08 12:13:20 -0700

    create custom popup styles and transform functional component into class component to track currently selected trail segment

--- client/src/components/TrailMap.js
+++ client/src/components/TrailMap.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { setState } from 'react';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 import { trailData } from '../data/data.js';
 // import styled from 'styled-components';
@@ -9,31 +9,74 @@ import '../index.css';
 //   height: ${props => props.height};
 // `;
 
-export default function TrailMap() {
+const featureStyle = {
+  color: '#749774'
+}
 
-  // const [activeTrail, setActiveTrail] = useState(null);
+const popupOptions = {
+  className: 'custom-popup'
+}
 
-  const featureStyle = {
-    color: '#749774'
+class TrailMap extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSegment: null,
+      otherSegmentsOfSameName: null
+    }
   }
 
-  const onEachFeature = (feature, layer) => {
-    const popupContent = `
-      <h4>${feature.properties.Name}</h4>
-      <p>${feature.properties.Miles} miles<p>
-      <p>${feature.properties.Type}<p>
-      <p>surface: ${feature.properties.Surface}<p>
-    `;
-    layer.bindPopup(popupContent);
-};
+  selectTrail = (feature) => {
+    return {
+      color: '#111111'
+    }
+  };
 
-  return (
-    <Map center={[45.5252, -122.7163]} zoom={13}>
-      <TileLayer 
-        url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-        maxZoom={20}
-      />
-      <GeoJSON data={trailData} style={featureStyle} onEachFeature={onEachFeature} />
-    </Map>
-  );
+  deselectTrail = (feature) => {
+    return {
+      color: '#749774'
+    }
+  }
+
+  onEachFeature = (feature, layer) => {
+    const popupContent = `
+      <strong>${feature.properties.Name}</strong><br/>
+      ${feature.properties.Type}<br/>
+      surface: ${feature.properties.Surface}<br/>
+      ${feature.properties.Miles} miles
+    `;
+    layer.bindPopup(popupContent, popupOptions);
+    layer.on({
+      // 'mouseover': function (event) {
+      //   changeColor(event.target);
+      // },
+      // 'mouseout': function (event) {
+      //   resetColor(event.target);
+      // },
+      'click': (event) => {
+        if (this.selectedSegment != null) {
+          const activeTrail = this.selectedSegment;
+          activeTrail.setStyle(this.deselectTrail(activeTrail));
+        }
+        let selection = event.target;
+        this.setState({selectedSegment: selection});
+        selection.setStyle(this.selectTrail(selection));
+      }
+    });
+  };
+
+  render() {
+    return (
+      <Map center={[45.5252, -122.7163]} zoom={13}>
+        <TileLayer 
+          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+          maxZoom={20}
+        />
+        <GeoJSON data={trailData} style={featureStyle} onEachFeature={this.onEachFeature} />
+      </Map>
+    );
+  }
 }
+
+export default TrailMap;
