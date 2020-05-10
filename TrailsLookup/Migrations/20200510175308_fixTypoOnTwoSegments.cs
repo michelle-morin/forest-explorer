0815112eb0c1b9d6d4ad3d7b9962dc521531fdd7
Michelle Morin <morin2m@gmail.com>
2020-05-10 11:05:02 -0700

    add new migration for typo correction in seed data

---
+++ TrailsLookup/Migrations/20200510175308_fixTypoOnTwoSegments.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrailsLookup.Migrations
+{
+    public partial class fixTypoOnTwoSegments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 149,
+                column: "Name",
+                value: "Saltzman Rd. / Powerline Area Path");
+
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 150,
+                column: "Name",
+                value: "Saltzman Rd. / Powerline Area Path");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 149,
+                column: "Name",
+                value: "Saltzmann Rd. / Powerline Area Path");
+
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 150,
+                column: "Name",
+                value: "Saltzmann Rd. / Powerline Area Path");
+        }
+    }
+}
