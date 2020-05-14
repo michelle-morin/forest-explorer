17cd1c7795868a0d4432b44e0ff2a9e5abd63cb8
Michelle Morin <morin2m@gmail.com>
2020-05-14 09:16:22 -0700

    convert functional component into class component, resolved issue regarding selected segment reverting to default color on mouseout

--- client/src/components/TrailMap.js
+++ client/src/components/TrailMap.js
@@ -1,60 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 import { trailData } from '../data/data.js';
-// import styled from 'styled-components';
 import '../index.css';
 
-// const Wrapper = styled.div`
-//   width: ${props => props.width};
-//   height: ${props => props.height};
-// `;
-
-function TrailMap() {
-
-  const [selectedTarget, changeTarget] = useState(null)
-  // const [otherSegmentsOfSameName, getCompleteTrail] = useState(null);
-
-  const featureStyle = {
-    color: '#749774'
-  }
-  const hoverStyle = {
-    color: '#d4af37'
-  }
-  const selectedStyle = {
-    color: '#0d120d'
-  }
-  const popupOptions = {
-    className: 'custom-popup'
+class TrailMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTarget: null,
+      otherSegmentsOfSameName: null
+    }
   }
 
-  const onEachFeature = (feature, layer) => {
-    const popupContent = `
-      <strong>${feature.properties.Name}</strong><br/>
-      ${feature.properties.Type}<br/>
-      surface: ${feature.properties.Surface}<br/>
-      ${feature.properties.Miles} miles
-    `;
-    layer.setStyle(featureStyle);
-    layer.bindPopup(popupContent, popupOptions);
-    layer.on({
-      'mouseover': function (event) {
-        layer.setStyle(hoverStyle);
-      },
-      'click': (event) => {
-        if (selectedTarget !== null) {
-          selectedTarget.setStyle(featureStyle);
+  render() {
+    const featureStyle = {
+      color: '#749774'
+    }
+    const hoverStyle = {
+      color: '#d4af37'
+    }
+    const selectedStyle = {
+      color: '#0d120d'
+    }
+    const popupOptions = {
+      className: 'custom-popup'
+    }
+
+    const onEachFeature = (feature, layer) => {
+      const popupContent = `
+        <strong>${feature.properties.Name}</strong><br/>
+        ${feature.properties.Type}<br/>
+        surface: ${feature.properties.Surface}<br/>
+        ${feature.properties.Miles} miles
+      `;
+      layer.setStyle(featureStyle);
+      layer.bindPopup(popupContent, popupOptions);
+      layer.on({
+        'mouseover': (event) => {
+          layer.setStyle(hoverStyle);
+        },
+        'click': (event) => {
+          if (this.state.selectedTarget !== null) {
+            this.state.selectedTarget.setStyle(featureStyle);
+          }
+          layer.setStyle(selectedStyle);
+          this.setState({selectedTarget: layer});
+        },
+        'mouseout': (event) => {
+          if (layer !== this.state.selectedTarget) {
+            layer.setStyle(featureStyle);
+          }
         }
-        layer.setStyle(selectedStyle);
-        changeTarget(layer);
-      },
-      'mouseout': function (event) {
-        if (layer !== selectedTarget) {
-          layer.setStyle(featureStyle);
-        }
-      }
-    });
-  };
-
+      });
+    };
+    
     return (
       <Map center={[45.545, -122.7163]} zoom={14}>
         <TileLayer 
@@ -65,5 +64,6 @@ function TrailMap() {
       </Map>
     );
   }
+}
 
 export default TrailMap;
