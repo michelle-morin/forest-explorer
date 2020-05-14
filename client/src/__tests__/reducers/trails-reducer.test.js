99ccf4d1b6ef22005f9f478b958f67f0de945de6
Michelle Morin <morin2m@gmail.com>
2020-05-14 09:41:53 -0700

    create second test for trailsReducer, receive meaningful fail

--- client/src/__tests__/reducers/trails-reducer.test.js
+++ client/src/__tests__/reducers/trails-reducer.test.js
@@ -1,6 +1,9 @@
-import trailsReducer from '../../reducers/trails-reducer';
+import trailsReducer from './../../reducers/trails-reducer';
+import * as c from './../../actions/ActionTypes';
 
 describe('trailsReducer', () => {
+  let action;
+
   const initialState = {
     isLoading: false,
     trails: [],
@@ -8,13 +11,22 @@ describe('trailsReducer', () => {
   };
 
   it('should successfully return the default state if no action type is passed to the reducer', () => {
-    expect(trailsReducer(initialState, { type: null })).toEqual(
-      {
-        isLoading: false,
-        trails: [],
-        error: null
-      }
-    );
+    expect(trailsReducer(initialState, { type: null })).toEqual({
+      isLoading: false,
+      trails: [],
+      error: null
+    });
+  });
+
+  test('requesting trails should successfully change isLoading from false to true', () => {
+    action = {
+      type: c.REQUEST_TRAILS
+    };
+    expect(trailsReducer(initialState, action)).toEqual({
+      isLoading: true,
+      trails: [],
+      error: null
+    });
   });
 
 });
