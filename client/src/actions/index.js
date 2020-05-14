b942082cf1d179f315b5cd7f5532037f0fc3b7b4
Michelle Morin <morin2m@gmail.com>
2020-05-14 10:08:59 -0700

    add action for making API call to custom backend TrailsLookup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,3 +13,17 @@ export const getTrailsFailure = (error) => ({
   type: c.GET_TRAILS_FAILURE,
   error
 });
+
+export const getAllTrailsFromApi = () => {
+  return (dispatch) => {
+    dispatch(requestTrails);
+    return fetch("http://localhost:5000/api/Trails")
+      .then(response => response.json())
+      .then((jsonifiedResponse) => {
+        dispatch(getTrailsSuccess(jsonifiedResponse));
+      })
+      .catch((error) => {
+        dispatch(getTrailsFailure(error));
+      });
+  }
+};
