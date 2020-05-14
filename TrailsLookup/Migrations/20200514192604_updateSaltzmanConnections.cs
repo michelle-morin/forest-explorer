eabe9f3839da92a1a76c3c059075192e33fdbd87
Michelle Morin <morin2m@gmail.com>
2020-05-14 12:27:02 -0700

    add migration for updates to seed data

---
+++ TrailsLookup/Migrations/20200514192604_updateSaltzmanConnections.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrailsLookup.Migrations
+{
+    public partial class updateSaltzmanConnections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 118,
+                column: "SecondAdjoiningSegment3",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 119,
+                column: "FirstAdjoiningSegment3",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 122,
+                column: "FirstAdjoiningSegment3",
+                value: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 118,
+                column: "SecondAdjoiningSegment3",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 119,
+                column: "FirstAdjoiningSegment3",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 122,
+                column: "FirstAdjoiningSegment3",
+                value: null);
+        }
+    }
+}
