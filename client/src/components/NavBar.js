66937252d5601a1e70089f7451b54bc26edeaadc
Michelle Morin <morin2m@gmail.com>
2020-05-15 14:08:00 -0700

    update navbar to use bootstrap components

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import styled from 'styled-components';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function NavBar() {
   return (
-    <React.Fragment>
-      <h1>Forest Explorer</h1>
-      <Link to="/">Home</Link>
-      <Link to="/explore">Explore</Link>
-      <Link to="/plan">Plan</Link>
-    </React.Fragment>
+    <header>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar.Brand as={Link} to="/">Forest Explorer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/explore">Explore</Nav.Link>
+            <Nav.Link as={Link} to="/plan">Plan</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
   );
 }
 
