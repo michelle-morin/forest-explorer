868c35956cd05c9f38566b23ef7852c6bce0720d
Michelle Morin <morin2m@gmail.com>
2020-05-15 14:07:38 -0700

    import bootstrap css

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import trailsReducer from './reducers/trails-reducer';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(trailsReducer, applyMiddleware(thunkMiddleware));
 
