9d85de1676c746739e1ae8799be9f152d23964f2
Michelle Morin <morin2m@gmail.com>
2020-05-16 19:38:50 -0700

    create component for tree image

---
+++ client/src/components/TreeAsset.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import tree from './../assets/tree.png';
+
+function TreeAsset() {
+  return (
+    <img src={tree} alt="watercolor tree" />
+  );
+}
+
+export default TreeAsset;
