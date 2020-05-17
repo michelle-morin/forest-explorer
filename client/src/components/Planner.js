4b1e77b26c148971fd99f726cb99b503122e7e35
Michelle Morin <morin2m@gmail.com>
2020-05-16 19:46:56 -0700

    create component to hold navbar and route builder

---
+++ client/src/components/Planner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+import RouteBuilder from './RouteBuilder';
+
+function Planner() {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <RouteBuilder />
+    </React.Fragment>
+  );
+}
+
+export default Planner;
