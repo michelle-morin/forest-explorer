6c25bce3a0b950cf22e95b396279c5c2d2b7b408
Michelle Morin <morin2m@gmail.com>
2020-05-16 22:37:45 -0700

    import react-typist

---
+++ client/src/components/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typist from 'react-typist';
+import './../index.css';
+
+function AboutPage() {
+  return (
+    <React.Fragment>
+      <h1>Welcome</h1>
+      <Typist>
+        <p>[about section]</p>
+      </Typist>
+    </React.Fragment>
+  );
+}
+
+export default AboutPage;
