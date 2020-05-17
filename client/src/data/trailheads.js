7bab5060dae5a968e7e15c42d1fdd39a0e485788
Michelle Morin <morin2m@gmail.com>
2020-05-17 12:11:01 -0700

    Create geojson data for trailheads in forest park

---
+++ client/src/data/trailheads.js
@@ -0,0 +1,325 @@
+export const trailheads = [
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 307,
+        "GeoObjNumber": 1,
+        "Name": "Wildwood-Newberry Trailhead",
+        "Type": "Trailhead",
+        "Description": "Trailhead access for the end (mile 30.1) of Wildwood Trail, which is only open to pedestrian use. Limited parking at trailhead."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.82351019545054,
+          45.60556845885819
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 308,
+        "GeoObjNumber": 2,
+        "Name": "Newton Road Trailhead",
+        "Type": "Trailhead",
+        "Description": "Trailhead access to Newton Road and Firelane 10, both of which are open to pedestrian, bicycle, and equestrian use. It also provides access to Wildwood Trail via a short access trail. Wildwood Trail is only open to pedestrian use."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.80235975105647,
+          45.59167335840418
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 309,
+        "GeoObjNumber": 3,
+        "Name": "Germantown: Wildwood",
+        "Type": "Trailhead",
+        "Description": "Trailhead access to the Wildwood Trail, which is only open to pedestrians. You can travel either direction on Wildwood - north (toward Newberry Road) or south (toward downtown). Parking is available on both sides of Germantown Road. This trailhead also provides access to Cannon Trail, which is only open to pedestrians."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.79405143960618,
+          45.587575704454935
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 310,
+        "GeoObjNumber": 4,
+        "Name": "Germantown: Leif Erikson",
+        "Type": "Trailhead",
+        "Description": "Access to the far end of Leif Erikson Drive, which is open to pedestrian, bicycle, and equestrian use. Parking available in lot and along Germantown road. Trailhead also provides access the Cannon Trail (only open to pedestrians) and nearby Firelane 9."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.7903330826765,
+          45.5892500758658
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 311,
+        "GeoObjNumber": 5,
+        "Name": "Springville Road Trailhead",
+        "Type": "Trailhead",
+        "Description": "Provides access to Springville Road and Firelane 7, both of which are open to pedestrian, bicycle, and equestrian use. Also nearby are the Hardesty, Trillium, and Ridge Trails, which are only open to pedestrian use."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.78845937483605,
+          45.57450153133901
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 312,
+        "GeoObjNumber": 6,
+        "Name": "Ridge Trailhead",
+        "Type": "Trailhead",
+        "Description": "Trailhead access for the Ridge Trail, which is only open to pedestrian use. To find the trailhead, park at the obvious pullout on the way down Bridge Ave (heading toward Portland). From there, walk back up Bridge Ave roughly .2 mile to find the actual trailhead, which is closer to the bridge's western terminus."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.77029423068298,
+          45.58436496670876
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 313,
+        "GeoObjNumber": 7,
+        "Name": "Upper Saltzman Road & Firelane 5",
+        "Type": "Trailhead",
+        "Description": "Provides access to Saltzman Rd and Firelane 5, both of which are open to pedestrian and bicycle use. Limited parking available."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.7834559886964,
+          45.56214990865814
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 314,
+        "GeoObjNumber": 8,
+        "Name": "Lower Saltzman Road Trailhead",
+        "Type": "Trailhead",
+        "Description": "Access to Saltzman Road, which climbs through the park up to NW Skyline Blvd. Saltzman Road is open to pedestrians and bicycles. This trailhead is also access to Firelane 4 and the most direct access to the Maple Trail, which is only open to pedestrians."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.7534726634385,
+          45.56635506086206
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 315,
+        "GeoObjNumber": 9,
+        "Name": "Forest Lane Trailhead",
+        "Type": "Trailhead",
+        "Description": "Trailhead access to Firelane 1, which is open to pedestrian, bicycle, and equestrian use. Follow Forest Lane until it dead ends at a gate, and find parking along the road (without blocking the gate). Note: Equestrian access with a trailer can be problematic, as there is no suitable place to turn around."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.7459682112168,
+          45.54519514750215
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 316,
+        "GeoObjNumber": 10,
+        "Name": "NW 53rd: Wildwood",
+        "Type": "Trailhead",
+        "Description": "This trailhead provides direct access to Wildwood Trail, close to the 9.25 mile marker. On-street parking can be found along both sides of NW 53rd."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.73687949465162,
+          45.54033116679266
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 317,
+        "GeoObjNumber": 11,
+        "Name": "NW 53rd: Dogwood & Wild Cherry",
+        "Type": "Trailhead",
+        "Description": "Trailhead access for Dogwood and Wild Cherry trails, both of which are only open to pedestrian use."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.73415650609105,
+          45.53844298401638
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 318,
+        "GeoObjNumber": 12,
+        "Name": "NW 53rd Ave: Birch Trail/Holman Lane",
+        "Type": "Trailhead",
+        "Description": "The trailhead for Birch Trail, which is only open to pedestrian use. Birch Trail intersects Wildwood Trail in .22 miles. Trailhead also provides access to Holman Lane, which is open to pedestrians and bicycles (riding uphill only)."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.73324193040197,
+          45.53364746863826
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 319,
+        "GeoObjNumber": 13,
+        "Name": "Thurman: Leif Erikson Drive",
+        "Type": "Trailhead",
+        "Description": "Provides access to Leif Erikson Drive, which is open to pedestrian and bicycle use. Also nearby is the Wild Cherry Trail, which is only open to pedestrian use."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.72474273414034,
+          45.53955217711908
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 320,
+        "GeoObjNumber": 14,
+        "Name": "Aspen Trailhead",
+        "Type": "Trailhead",
+        "Surface": "This trailhead provides access to the Aspen Trail, which is only open to pedestrians. Aspen Trail connects to Wildwood Trail in .23 miles."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.71882098678677,
+          45.536284619415405
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 321,
+        "GeoObjNumber": 15,
+        "Name": "Lower Macleay Park Entrance",
+        "Type": "Trailhead",
+        "Surface": "Provides access to the Lower Macleay Trail, which is only open to pedestrian use. This trail parallels Balch Creek, one of Forest Park's few year-round streams, which supports a resident population of cutthroat trout. Lower Macleay Trail intersects Wildwood Trail in .84 mile, at the location of the Stone House, a remnant rest station from the WPA. On-street parking is available throughout the surrounding neighborhood."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.71244724041996,
+          45.53583077735856
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 322,
+        "GeoObjNumber": 16,
+        "Name": "Holman Lane Entrance",
+        "Type": "Trailhead",
+        "Surface": "Provides access the lower end of Holman Lane, which is open to pedestrians and bicycles (riding uphill only). There is a maintained meadow near the trailhead, a unique feature in Forest Park."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.7181642230927,
+          45.53370566393555
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 323,
+        "GeoObjNumber": 17,
+        "Name": "Tunnel & Cumberland Trails Entrance",
+        "Type": "Trailhead",
+        "Surface": "Trailhead access for the Tunnel and Cumberland trails, both of which are only open to pedestrian use. Limited off-street parking available."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.71717529923743,
+          45.530408311609364
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 324,
+        "GeoObjNumber": 18,
+        "Name": "Upper Macleay Park Entrance",
+        "Type": "Trailhead",
+        "Surface": "Trailhead access to Wildwood Trail, which is only open to pedestrian use. Parking is only available on the north side of the road. You can travel in either direction on the Wildwood - north (dropping into Balch Creek) or south (cross NW Cornell and begin the steep climb to Pittock Mansion)."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.72672459103194,
+          45.52687788222641
+        ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+        "TrailId": 325,
+        "GeoObjNumber": 19,
+        "Name": "Pittock Mansion site",
+        "Type": "Trailhead",
+        "Surface": "Access to Wildwood Trail heading north and south from Pittock Mansion. Wildwood trail quickly loses elevation in both directions."
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.71827896131065,
+          45.5252667479634
+        ]
+    }
+  }
+]
