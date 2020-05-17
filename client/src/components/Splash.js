23978b4e7ecb4a95f8f9b8a028ea9652f2f10091
Michelle Morin <morin2m@gmail.com>
2020-05-16 19:39:07 -0700

    import tree and path assets

--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import TreeAsset from './TreeAsset';
 import styled from 'styled-components';
+import path from './../assets/path.png';
+
+const SplashWrapper = styled.div`
+  height: 90vh;
+  width: 100%;
+  background-image: url(${path});
+  background-position: center;
+  background-size: cover;
+  background-attatchment: fixed;
+`;
 
 function Splash() {
   return (
-    <h1>splash</h1>
+    <SplashWrapper>
+      <TreeAsset />
+    </SplashWrapper>
   );
 }
 
