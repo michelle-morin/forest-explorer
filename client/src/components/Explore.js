baa1df3e85802253e328b445d26a06c618365498
Michelle Morin <morin2m@gmail.com>
2020-05-16 19:46:39 -0700

    Create component to hold trail map and nav bar

---
+++ client/src/components/Explore.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+import TrailMap from './TrailMap';
+
+function Explore() {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <TrailMap />
+    </React.Fragment>
+  );
+}
+
+export default Explore;
