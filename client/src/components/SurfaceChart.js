05f49e02718dfd89fc8f010388b866df74bf7610
Michelle Morin <morin2m@gmail.com>
2020-05-17 21:37:57 -0700

    add inner label to chart displaying total mileage

--- client/src/components/SurfaceChart.js
+++ client/src/components/SurfaceChart.js
@@ -3,22 +3,25 @@ import { VictoryPie, VictoryLabel } from 'victory';
 import PropTypes from 'prop-types';
 
 function SurfaceChart(props) {
-
+  const roundedDistance = parseFloat((props.totalDistance).toFixed(3));
   return (
-    <React.Fragment>
+    <svg width={500} height={500}>
       <VictoryPie
+        standalone={false}
         colorScale={["#008f68", "#EFBB35", "#6DB65B", "#4AAE9B", "#EF6D35"]}
         data={props.data}
-        width={400} height={400}
-        innerRadius={70}
+        width={500}
+        height={500}
+        innerRadius={125}
       />
       <VictoryLabel
         textAnchor="middle"
+        verticalAnchor="middle"
         style={{ fontSize: 20 }}
-        x={200} y={200}
-        text={props.totalDistance}
+        x={250} y={250}
+        text={`${roundedDistance} miles`}
       />
-    </React.Fragment>
+    </svg>
   )
 }
 
