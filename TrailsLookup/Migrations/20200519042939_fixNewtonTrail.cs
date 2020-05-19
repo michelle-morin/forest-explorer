09efa82fc830c275334b8d8119b5b0f1210702ce
Michelle Morin <morin2m@gmail.com>
2020-05-18 21:30:27 -0700

    add new migration for recent database seed data change

---
+++ TrailsLookup/Migrations/20200519042939_fixNewtonTrail.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrailsLookup.Migrations
+{
+    public partial class fixNewtonTrail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 254,
+                columns: new[] { "Surface", "Type" },
+                values: new object[] { "soil", "Trail/Path (6-12 ft.)" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 254,
+                columns: new[] { "Surface", "Type" },
+                values: new object[] { "Trail/Path (6-12 ft.)", "soil" });
+        }
+    }
+}
