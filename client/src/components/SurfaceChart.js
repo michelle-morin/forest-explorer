66fea1b148eb384ffcfda7bcf92bb0f5c2126390
Michelle Morin <morin2m@gmail.com>
2020-05-18 21:54:48 -0700

    update props passed to victorypie and victorycontainer

--- client/src/components/SurfaceChart.js
+++ client/src/components/SurfaceChart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VictoryPie, VictoryLabel, VictoryContainer } from 'victory';
 import PropTypes from 'prop-types';
+import '../index.css';
 
 function SurfaceChart(props) {
   const roundedDistance = parseFloat((props.totalDistance).toFixed(3));
@@ -10,16 +11,14 @@ function SurfaceChart(props) {
         standalone={false}
         colorScale={["rgb(238,193,175)", "rgb(201,221,188)", "rgb(146,169,219)", "rgb(213,215,232)"]}
         data={props.data}
-        width={300}
-        height={300}
-        innerRadius={60}
-        style={{ labels: { fill: "#111111", fontSize: 18, padding: 4, fontFamily: "'Montserrat', sans-serif" } }}
+        innerRadius={100}
+        style={{ labels: { fill: "#111111", fontSize: 18, padding: 2, fontFamily: "'Montserrat', sans-serif" } }}
       />
       <VictoryLabel
         textAnchor="middle"
         verticalAnchor="middle"
         style={{ fontSize: 24, fontFamily: "'Montserrat', sans-serif" }}
-        x={150} y={150}
+        x={200} y={200}
         text={`${roundedDistance}\nmiles`}
       />
     </VictoryContainer>
