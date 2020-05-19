46c5e2844284ae6092f4aa90e13a38217fbab692
Michelle Morin <morin2m@gmail.com>
2020-05-18 23:18:44 -0700

    change color scheme of pie chart

--- client/src/components/SurfaceChart.js
+++ client/src/components/SurfaceChart.js
@@ -9,7 +9,7 @@ function SurfaceChart(props) {
     <VictoryContainer width={400} height={400}>
       <VictoryPie
         standalone={false}
-        colorScale={["rgb(238,193,175)", "rgb(201,221,188)", "rgb(146,169,219)", "rgb(213,215,232)"]}
+        colorScale={["rgb(95,188,228)", "rgb(73,161,105)", "rgb(228,212,121)", "rgb(220,114,48)"]}
         data={props.data}
         innerRadius={100}
         style={{ labels: { fill: "#111111", fontSize: 18, padding: 2, fontFamily: "'Montserrat', sans-serif" } }}
