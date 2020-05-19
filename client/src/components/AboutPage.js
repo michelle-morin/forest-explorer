49d5df134a9160213031b6842432a5b1de8f87cc
Michelle Morin <morin2m@gmail.com>
2020-05-18 20:49:10 -0700

    import navbar on about page component

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import NavBar from './NavBar';
 import Typist from 'react-typist';
 import styled from 'styled-components';
 import './../index.css';
 
 const AboutWrapper = styled.div`
   font-family: 'Montserrat', sans-serif;
+  position: absolute;
+  top: 15vh;
+  padding: 2%;
+  font-size: 2rem;
 `;
 
 function AboutPage() {
   return (
-    <AboutWrapper>
-      <h1>Welcome</h1>
-      <Typist>
-        <p>[about section]</p>
-      </Typist>
-    </AboutWrapper>
+    <React.Fragment>
+      <NavBar />
+      <AboutWrapper>
+        <Typist>
+          <p>Welcome!</p>
+        </Typist>
+      </AboutWrapper>
+    </React.Fragment>
   );
 }
 
