8dc03503070253e7bcd45c33e52a68da6b45cc25
Michelle Morin <morin2m@gmail.com>
2020-05-20 12:13:01 -0700

    add migration for updating wildwood trail (trailid 1)

--- TrailsLookup/Migrations/20200520191215_updateWildwoodTrail.Designer.cs
+++ TrailsLookup/Migrations/20200520191215_updateWildwoodTrail.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrailsLookup.Models;
 
 namespace TrailsLookup.Migrations
 {
     [DbContext(typeof(TrailsLookupContext))]
-    partial class TrailsLookupContextModelSnapshot : ModelSnapshot
+    [Migration("20200520191215_updateWildwoodTrail")]
+    partial class updateWildwoodTrail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TrailsLookup/Migrations/20200520191215_updateWildwoodTrail.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrailsLookup.Migrations
+{
+    public partial class updateWildwoodTrail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 1,
+                columns: new[] { "FirstAdjoiningSegment1", "FirstAdjoiningSegment2" },
+                values: new object[] { 934, 446 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trails",
+                keyColumn: "TrailId",
+                keyValue: 1,
+                columns: new[] { "FirstAdjoiningSegment1", "FirstAdjoiningSegment2" },
+                values: new object[] { 1, null });
+        }
+    }
+}
