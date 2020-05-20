1d28322c8de55fa56b320ec251e402848fcbdadd
Michelle Morin <morin2m@gmail.com>
2020-05-20 15:25:13 -0700

    reworded somewhat confusing phrase

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -39,7 +39,7 @@ function AboutPage() {
         <Typing>
           <p>Welcome!</p>
           <p>This is a passion project fueled by my love of trail running.</p>
-          <p>Portland's west hills include a vast trail network. Forest Park spans over 5,100 acres along Portland's northwest urban boundary. Trails within the Hoyt Aboretum weave between 2,300 species of trees and shrubs from six continents. In addition to forest, Washington Park includes an immaculate rose test garden, a zoo, and museums.</p>
+          <p>Portland's west hills include a vast trail network. Forest Park spans over 5,100 acres along Portland's northwest urban boundary. Trails within the Hoyt Aboretum weave between 2,300 species of trees and shrubs from six continents. In addition to forested trails, Washington Park includes an immaculate rose test garden, a zoo, and museums.</p>
           <p>While this application encourages trail use, please be mindful of your impact on the trails and fragile ecosystems of our urban wilderness.</p>
         </Typing>
       </AboutWrapper>
